00bc242a37f50dfc787fe1c8829b1225fffc476f
Michael Small <chickenhat22@gmail.com>
2024-08-11 13:01:01 -0500

    docs: example of add/delete book in withDataService

--- src/app/store/books-wth-data-service.component.ts
+++ src/app/store/books-wth-data-service.component.ts
@@ -1,18 +1,39 @@
 import {Component, effect, inject, OnInit} from '@angular/core';
 import {BooksStore} from "./books.store";
+import {Book} from "./books.model";
+import {AddBookComponent} from "../components/add-book.component";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-books-with-data',
   standalone: true,
-  template: `withDataService`
+  imports: [
+    AddBookComponent,
+    JsonPipe
+  ],
+  template: `
+    <app-add-book (submit)="addBook($event)" />
+
+    @for (book of $books(); track $index) {
+      <pre>{{ book | json }}: <button (click)="deleteBook(book)">delete</button></pre>
+    } @empty {
+      <p>No books</p>
+    }
+  `
 })
 
 export class BooksWithDataComponent implements OnInit {
   booksWithDataStore = inject(BooksStore);
+  $books = this.booksWithDataStore.books;
   ngOnInit() {
     this.booksWithDataStore.loadBooks();
   }
-  booksLoad = effect(() => {
-    console.log(this.booksWithDataStore.books());
-  })
+
+  addBook(book: Book) {
+    this.booksWithDataStore.addBook({id: crypto.randomUUID(), author: 'jerry', name: 'hey'}).unsubscribe()
+  }
+
+  deleteBook(book: Book) {
+    this.booksWithDataStore.deleteBook(book);
+  }
 }
